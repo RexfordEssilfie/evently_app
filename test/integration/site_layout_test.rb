413948dfb941f017f5abc73fae4c2da0784841e8
Rexford Essilfie <rexfordessilfie09@gmail.com>
2020-09-14 21:24:47 -0500

    Add tests for layout titles on all pages

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,12 +8,28 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", root_path, count: 3
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", dynamic_path
+  end
+
+  test "layout titles" do
+    get root_path
+    assert_select "title", full_title()
+
     get contact_path
     assert_select "title", full_title("Contact")
+
+    get help_path
+    assert_select "title", full_title("Help")
+
+    get about_path
+    assert_select "title", full_title("About")
+
+    get dynamic_path
+    assert_select "title", full_title("Random Verse")
   end
 
 end
