0706111b66b5ea6b5ad1c3c666dcdd6c1d1cf4fa
Rexford Essilfie <rexfordessilfie09@gmail.com>
2020-10-05 20:35:02 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_033450) do
+ActiveRecord::Schema.define(version: 2020_10_06_013038) do
+
+  create_table "events", force: :cascade do |t|
+    t.text "name"
+    t.text "description"
+    t.time "start_time"
+    t.time "end_time"
+    t.text "visibility"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
