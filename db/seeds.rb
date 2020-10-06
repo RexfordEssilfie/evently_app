14afd66a4d83ed6fd0bbe7e14e7b921f522f95b8
Rexford Essilfie <rexfordessilfie09@gmail.com>
2020-10-05 20:20:13 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Loading fake users into the database
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'fake_users.csv'))
+csv = CSV.parse(csv_text, :force_quotes => true, :headers => true, :quote_char => "'", :encoding => Encoding::UTF_8)
+File.open(Rails.root.join('lib', 'seeds', 'fake_users.csv')) do |f|
+  f.each_line do |row|
+  columns = row.split(",")
+  t = User.new
+  t.name = columns[0]
+  t.email = columns[1]
+  t.password = columns[2]
+  t.password_confirmation = columns[3]
+  t.save
+  puts columns[2..3]
+  puts t.errors.full_messages
+
+  if t.valid?
+    puts "#{t.name}, #{t.email} saved"
+  else
+    puts "#{t.name}, #{t.email} not saved"
+  end
+
+end
+end
+
+puts "There are now #{User.count} rows in the users table"
