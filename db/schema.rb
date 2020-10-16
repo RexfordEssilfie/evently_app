c7aecc3cc7377fd7b1ea7e2c1ae92e307ffa2677
Rexford Essilfie <rexfordessilfie09@gmail.com>
2020-10-16 07:31:18 -0500

    Drop and readd time columns for events

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_120923) do
+ActiveRecord::Schema.define(version: 2020_10_16_122730) do
 
   create_table "events", force: :cascade do |t|
     t.text "name"
     t.text "description"
-    t.datetime "start_time"
-    t.datetime "end_time"
     t.text "visibility"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
     t.string "location"
+    t.datetime "start_time"
+    t.datetime "end_time"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
