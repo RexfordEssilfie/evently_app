c6f19912fb1d3f9357e68756edcd0f10e17ec2d9
Rexford Essilfie <rexfordessilfie09@gmail.com>
2020-10-16 07:30:52 -0500

    Remove bad migration

--- db/migrate/20201016120923_change_data_type_for_start_time_and_end_time.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDataTypeForStartTimeAndEndTime < ActiveRecord::Migration[6.0]
-  def change
-    change_column :events, :end_time, :datetime
-    change_column :events, :start_time, :datetime
-  end
-end
