28f218a1721a6eda744795e88c29f260a460c429
Rexford Essilfie <rexfordessilfie09@gmail.com>
2020-10-16 06:34:24 -0500

    Remove comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)    # Not the final implementation!
+    @user = User.new(user_params)    
     if @user.save
       flash[:success] = "Welcome to Evently!"
       redirect_to @user #or user_url(@user)
