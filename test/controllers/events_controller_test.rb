09e2a3127cdcce47785296e83dde198c3693a33b
Rexford Essilfie <rexfordessilfie09@gmail.com>
2020-10-16 06:35:59 -0500

    Tests to green :)

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -2,47 +2,48 @@ require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @event = events(:one)
+    @event = Event.first
+    @user = User.first
   end
 
   test "should get index" do
-    get events_url
+    get events_path
     assert_response :success
   end
 
   test "should get new" do
-    get new_event_url
+    get new_event_path
     assert_response :success
   end
 
   test "should create event" do
     assert_difference('Event.count') do
-      post events_url, params: { event: { description: @event.description, end_time: @event.end_time, name: @event.name, start_time: @event.start_time, visibility: @event.visibility } }
+      post events_path, params: { event: {email: @user.email, password: "foobar", description: @event.description, end_time: @event.end_time, name: @event.name, start_time: @event.start_time, visibility: @event.visibility, location: @event.location } }
     end
 
-    assert_redirected_to event_url(Event.last)
+    assert_redirected_to event_path(Event.last)
   end
 
   test "should show event" do
-    get event_url(@event)
+    get event_path(@event)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_event_url(@event)
+    get edit_event_path(@event)
     assert_response :success
   end
 
   test "should update event" do
-    patch event_url(@event), params: { event: { description: @event.description, end_time: @event.end_time, name: @event.name, start_time: @event.start_time, visibility: @event.visibility } }
-    assert_redirected_to event_url(@event)
+    patch event_path(@event), params: { event: { description: @event.description, end_time: @event.end_time, name: @event.name, start_time: @event.start_time, visibility: @event.visibility } }
+    assert_redirected_to event_path(@event)
   end
 
   test "should destroy event" do
     assert_difference('Event.count', -1) do
-      delete event_url(@event)
+      delete event_path(@event)
     end
 
-    assert_redirected_to events_url
+    assert_redirected_to events_path
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = User.first
+  end
+
+  test "should get index" do
+    get users_path
+    assert_response :success
+  end
+
   test "should get new" do
-    get signup_path #this path is automatically generated when we put the get operation in the routes
+    get new_user_path
+    assert_response :success
+  end
+
+  test "should show user" do
+    get user_path(@user)
     assert_response :success
   end
 
