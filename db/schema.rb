0e63145d648218fea710fa306136a38865c25623
Rexford Essilfie <rexfordessilfie09@gmail.com>
2020-10-16 03:04:46 -0500

    Add location to events model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_062313) do
+ActiveRecord::Schema.define(version: 2020_10_16_071115) do
 
   create_table "events", force: :cascade do |t|
     t.text "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_10_06_062313) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.string "location"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
